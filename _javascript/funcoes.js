d39a8d0c30e5265883fdaaae50913340cf4dde3e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-09-13 20:17:23 -0300

    Projeto criado

---
+++ _javascript/funcoes.js
@@ -0,0 +1,3 @@
+function mudaFoto(foto) { /* funçoes javascript,. area de script, criação da função "function" chamada mudaFoto vai receber um parametro (foto)*/
+    document.getElementById("icone").src = foto; /* função para mudar a foto da barra de menu.*/
+}
